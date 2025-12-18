a3b724a02663d89b18550184e68634f0ea165a26
u011317173 <u011317173@noreply.gitcode.com>
2025-12-18 06:52:48 +0000

    del print

--- ops/ascendc/src/sobel_custom/op_kernel/sobel_custom_base.h
+++ ops/ascendc/src/sobel_custom/op_kernel/sobel_custom_base.h
@@ -12,8 +12,6 @@ __aicore__ inline T Min(T a, T b)
 template <typename T, typename U>
 __aicore__ inline T CeilDiv(T x, U y)
 {
-    // return y == 0 ? x : (x + y - 1) / y;
-    // return y == 0 ? x : 1 + ((x - 9) + 7 - 1) / 7;
     return y == 0 ? x : (1 + ((x - y) + (y - 2) - 1) / (y - 2));
 }
 
