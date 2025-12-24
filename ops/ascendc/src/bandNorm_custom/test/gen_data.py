bb32f21e1d6e2a4c39db1c631ca07091115b8d7c
AuriveDev <AuriveDev@noreply.gitcode.com>
2025-12-24 11:30:22 +0000

    bandnorm gen_data

--- ops/ascendc/src/bandNorm_custom/test/gen_data.py
+++ ops/ascendc/src/bandNorm_custom/test/gen_data.py
@@ -3,11 +3,11 @@
 import numpy as np
 import torch
 
-def cal_band_power(real, image):
+def cal_band_power(x1, x2):
     eps = torch.finfo(torch.float32).eps
-    real_square = np.square(real)
-    image_square = np.square(image)
-    power_1 = real_square + image_square
+    x1_square = np.square(x1)
+    x2_square = np.square(x2)
+    power_1 = x1_square + x2_square
     power_2 = np.sum(power_1, axis=1)
     power = np.sqrt(power_2 + eps)
     return power
@@ -15,11 +15,11 @@ def cal_band_power(real, image):
 if __name__ == "__main__":
 
     input_shape = [2, 121, 47]
-    real_data = np.random.randn(*input_shape).astype(np.float16)
-    imag_data = np.random.randn(*input_shape).astype(np.float16)
-    power = cal_band_power(real_data, imag_data)
+    x1_data = np.random.randn(*input_shape).astype(np.float16)
+    x2_data = np.random.randn(*input_shape).astype(np.float16)
+    power = cal_band_power(x1_data, x2_data)
 
-    real_data.tofile("./real.bin")
-    imag_data.tofile("./imag.bin")
+    x1_data.tofile("./x1.bin")
+    x2_data.tofile("./x2.bin")
     power.tofile("./target.bin")
 
